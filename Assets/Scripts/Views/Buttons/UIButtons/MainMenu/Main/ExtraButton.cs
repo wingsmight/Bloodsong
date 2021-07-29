0bc3f6d54aaac5e47250cfb7763473b7a6c75398
wingsmight <wingsmight@gmail.com>
2021-07-29 20:09:42 +0700

    redisign subMenu of Main menu

--- Assets/Scripts/Views/Buttons/UIButtons/MainMenu/Main/ExtraButton.cs
+++ Assets/Scripts/Views/Buttons/UIButtons/MainMenu/Main/ExtraButton.cs
@@ -6,13 +6,11 @@ namespace StoryMenu
 {
     public class ExtraButton : StoryMenuButton
     {
-        [SerializeField] private FadeAnimation menuButtonsFade;
         [SerializeField] private ExtraMenuView extraMenu;
 
 
         protected override void OnClick()
         {
-            menuButtonsFade.Disappear();
             extraMenu.Show();
         }
     }
